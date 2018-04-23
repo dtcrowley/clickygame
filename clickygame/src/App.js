098373730dae3a10004053cdcc12dc4c3e5dca70
dtcrowley <crowleyd1988@gmail.com>
2018-04-23 19:44:32 -0400

    more changes

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import characters from "./characters.json";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+  state = {
+    images: images,
+    score: 0,
+    topScore: 0,
+    text: "",
+    clickedImages: []
+  };
+  
 }
-
-export default App;
